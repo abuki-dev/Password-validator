3459c4c8e5319a5944a1d86335996d0f70d77e4f
Abubeker <233910354+abuki-dev@users.noreply.github.com>
2026-04-29 18:06:05 +0300

    adding  dashboard

--- docs/src/main.js
+++ docs/src/main.js
@@ -199,7 +199,7 @@ if (document.getElementById("login-container")) {
     let toberemoved = PendingArray.indexOf(users);
     PendingArray.splice(toberemoved, 1);
     localStorage.setItem("Pending", JSON.stringify(PendingArray));
-    window.location.replace("../login");
+    window.location.replace("../Dashboard");
   }
   const form = document.getElementById("userForm");
 
@@ -288,62 +288,5 @@ if (document.getElementById("login-container")) {
     }
   });
 }
-//Only for login page
-if (document.getElementById("login-page")) {
-  const login = document.getElementById("login-form");
-
-  function trylogin() {
-    let exit = false;
-    const logingemail = document.getElementById("login-email").value;
-    const loginpasword = document.getElementById("login-password").value;
-    const logdata = localStorage.getItem("Logedin") || "[]";
-    if (logdata.length === 0) {
-      alert("pleas create account first");
-    } else {
-      const users = JSON.parse(logdata);
-      for (const user of users) {
-        if (user["Email"] === logingemail) {
-          if (user["Passkey"] === loginpasword) {
-            window.location.replace("../Users");
-            exit = true;
-          } else {
-            alert("password incorrct");
-            login.focus();
-          }
-        }
-      }
-    }
-    if (!exit) {
-      alert("No acount information create account first");
-    }
-  }
-  login.addEventListener("submit", (e) => {
-    e.preventDefault();
-    trylogin();
-  });
-  function togglePassword() {
-    const passwordInput = document.getElementById("login-password");
-    const eyeIcon = document.getElementById("eye-icon");
-
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      eyeIcon.innerHTML = `
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path>
-          `;
-    } else {
-      passwordInput.type = "password";
-      eyeIcon.innerHTML = `
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-          `;
-    }
-  }
-}
 
 //Only userpage
-if (document.getElementById("user-page")) {
-  const goback = document.getElementById("back-to-home");
-  goback.addEventListener("click", () => {
-    window.location.replace("../login");
-  });
-}
